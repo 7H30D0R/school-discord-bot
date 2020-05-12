f5ac26e40a6a73f4c80ed4c55656000ad1d4fa17
7H30D0R <marcusanholt@gmail.com>
2020-05-12 20:03:44 +0200

    Add MySQL port to environment file

--- src/config.js
+++ src/config.js
@@ -11,8 +11,9 @@ module.exports = {
     DB_CONFIG: {
         host: process.env.MYSQL_HOST,
         user: process.env.MYSQL_USER,
-        password: process.env.MYSQL_PASS,
-        database: process.env.MYSQL_DATABASE
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        port: process.env.MYSQL_PORT || 3306
     },
 
     REACTION_NUMBERS: [
