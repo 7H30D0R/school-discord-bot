f204860b1127cfad7acc27cb828972fef309c1f0
7H30D0R <marcusanholt@gmail.com>
2020-05-14 19:55:18 +0200

    Clean entry point (index.js)

--- src/index.js
+++ src/index.js
@@ -5,20 +5,11 @@ import Database from './classes/database';
 
 // Initialize database connection
 Database.connect(DB_CONFIG).then(async () => {
-    let quizzes = await Quizzes.inRandomOrder().first();
-
-    console.log(quizzes.id);
-
-    let questions = await quizzes.questions();
-
-    for (let question of questions) {
-        let answer = await question.correctAnswer();
-        console.log(`${question.caption} = ${answer.caption}`);
-    }
 
     // Initialize Discord client
     Client.initialize();
-}).catch((error) => { console.log("big error"); console.log(error); });
+
+}).catch((error) => { console.log(error); });
 
 
 
